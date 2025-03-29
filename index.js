10272a40ab61982add68b2f0169d24a32f5c53e1
januszek12309 <kunaszlajegg@gmail.com>
2025-03-29 18:31:59 +0100

    Zaktualizowane pliki

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 require('dotenv').config({ path: './.env' });
 console.log('VERIFY_TOKEN:', process.env.VERIFY_TOKEN); 
+
 const cors = require('cors');
+const webApp = express();  // Zadeklaruj webApp tutaj przed użyciem
+
+// Użyj cors po zadeklarowaniu webApp
 webApp.use(cors());
-const webApp = express();
 
 const PORT = process.env.PORT || 5000;
-
 
 // Logowanie zapytań
 webApp.use((req, res, next) => {
@@ -23,10 +25,9 @@ const fbWebhookRoute = require('./routes/fbWebhookRoute');
 const sendMessageRoute = require('./routes/sendMessageRoute');
 
 // Ustawiasz trasy
-// Ustawiasz trasy
 webApp.use('/webhook', fbWebhookRoute.router); // Endpoint do webhooka
 webApp.use('/', homeRoute.router);              // Strona główna
-webApp.use('/sendMessage', sendMessageRoute.router); // Endpoint do wysyłania wiadomości// Endpoint do wysyłania wiadomości
+webApp.use('/sendMessage', sendMessageRoute.router); // Endpoint do wysyłania wiadomości
 
 // Uruchamiasz serwer
 webApp.listen(PORT, () => {
