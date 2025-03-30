495e6ca5ea10f1747e8e475d5a6e0deb107b064d
januszek12309 <kunaszlajegg@gmail.com>
2025-03-30 17:08:22 +0200

    dhj

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
-require('dotenv').config({ path: './.env' });
-console.log('VERIFY_TOKEN:', process.env.VERIFY_TOKEN);
+require('dotenv').config({ path: './.env' });  // Ładowanie konfiguracji z pliku .env
+console.log('VERIFY_TOKEN:', process.env.VERIFY_TOKEN);  // Sprawdzenie tokenu weryfikacji w logach
+
 const cors = require('cors');
-const fbWebhookRoute = require('./routes/fbWebhookRoute'); // Importujesz tylko raz
+const fbWebhookRoute = require('./routes/fbWebhookRoute');  // Import webhooka
 
 const webApp = express();
-webApp.use(cors());  // Używanie CORS
+webApp.use(cors());  // Używanie CORS (dopuszcza połączenia z innych domen)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;  // Użyj portu z env lub 8080 jako domyślny
 
 // Logowanie zapytań
 webApp.use((req, res, next) => {
-  console.log(`Received ${req.method} request at ${req.path}`);
+  console.log(`Received ${req.method} request at ${req.path}`);  // Zapisuje metodę i ścieżkę
   next();
 });
 
+// Middleware do przetwarzania danych w formacie URL-encoded i JSON
 webApp.use(express.urlencoded({ extended: true }));
 webApp.use(express.json());
 
-// Importujesz swoje trasy
+// Importujesz trasy
 const homeRoute = require('./routes/homeRoute');
 const sendMessageRoute = require('./routes/sendMessageRoute');
 
@@ -29,5 +31,5 @@ webApp.use('/sendMessage', sendMessageRoute.router); // Endpoint do wysyłania w
 
 // Uruchamiasz serwer
 webApp.listen(PORT, () => {
-  console.log(`Server is up and running at http://localhost:${PORT}`);
+  console.log(`Server is up and running at http://localhost:${PORT}`);  // Uruchomienie serwera
 });
